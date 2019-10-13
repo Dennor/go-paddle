8db2155a8f298a038fce1d53ef04eaac303acdc4
Kamil Matysiewicz <kamil@aexol.com>
2019-10-13 09:22:25 +0200

    add subscription_payment_id field to payment_failed event

--- events/subscription/payment_failed.go
+++ events/subscription/payment_failed.go
@@ -9,25 +9,26 @@ const PaymentFailedAlertName = "subscription_payment_failed"
 
 // PaymentFailed refer to https://paddle.com/docs/subscriptions-event-reference/#subscription_payment_failed
 type PaymentFailed struct {
-	AlertID            int                     `json:"alert_id,string"`
-	AlertName          string                  `json:"alert_name"`
-	Amount             *types.CurrencyValue    `json:"amount,string"`
-	CancelURL          string                  `json:"cancel_url"`
-	CheckoutID         string                  `json:"checkout_id"`
-	Currency           string                  `json:"currency"`
-	Email              string                  `json:"email"`
-	EventTime          *types.Datetime         `json:"event_time,string"`
-	HardFailure        *types.PhpBool          `json:"hard_failure,string"`
-	MarketingConsent   *types.MarketingConsent `json:"marketing_consent,string"`
-	NextRetryDate      *types.Date             `json:"next_retry_date,string"`
-	Passthrough        string                  `json:"passthrough"`
-	Quantity           int                     `json:"quantity,string"`
-	Status             string                  `json:"status"`
-	SubscriptionID     int                     `json:"subscription_id,string"`
-	SubscriptionPlanID int                     `json:"subscription_plan_id,string"`
-	UnitPrice          *types.CurrencyValue    `json:"unit_price,string"`
-	UpdateURL          string                  `json:"update_url"`
-	PSignature         string                  `json:"p_signature" php:"-"`
+	AlertID               int                     `json:"alert_id,string"`
+	AlertName             string                  `json:"alert_name"`
+	Amount                *types.CurrencyValue    `json:"amount,string"`
+	CancelURL             string                  `json:"cancel_url"`
+	CheckoutID            string                  `json:"checkout_id"`
+	Currency              string                  `json:"currency"`
+	Email                 string                  `json:"email"`
+	EventTime             *types.Datetime         `json:"event_time,string"`
+	HardFailure           *types.PhpBool          `json:"hard_failure,string"`
+	MarketingConsent      *types.MarketingConsent `json:"marketing_consent,string"`
+	NextRetryDate         *types.Date             `json:"next_retry_date,string"`
+	Passthrough           string                  `json:"passthrough"`
+	Quantity              int                     `json:"quantity,string"`
+	Status                string                  `json:"status"`
+	SubscriptionID        int                     `json:"subscription_id,string"`
+	SubscriptionPaymentID int                     `json:"subscription_payment_id,string,omitempty"`
+	SubscriptionPlanID    int                     `json:"subscription_plan_id,string"`
+	UnitPrice             *types.CurrencyValue    `json:"unit_price,string"`
+	UpdateURL             string                  `json:"update_url"`
+	PSignature            string                  `json:"p_signature" php:"-"`
 }
 
 func (s *PaymentFailed) Serialize() ([]byte, error) {
