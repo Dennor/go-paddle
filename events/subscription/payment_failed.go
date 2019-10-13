f2da314686a89d7870b61a08cb208bba896e7c23
Kamil Matysiewicz <kamil@aexol.com>
2019-10-13 09:31:44 +0200

    Missing attempt_number, instalments and user_id in subscription_payment_failed event

--- events/subscription/payment_failed.go
+++ events/subscription/payment_failed.go
@@ -12,12 +12,14 @@ type PaymentFailed struct {
 	AlertID               int                     `json:"alert_id,string"`
 	AlertName             string                  `json:"alert_name"`
 	Amount                *types.CurrencyValue    `json:"amount,string"`
+	AttemptNumber         int                     `json:"attempt_number,string,omitempty"`
 	CancelURL             string                  `json:"cancel_url"`
 	CheckoutID            string                  `json:"checkout_id"`
 	Currency              string                  `json:"currency"`
 	Email                 string                  `json:"email"`
 	EventTime             *types.Datetime         `json:"event_time,string"`
-	HardFailure           *types.PhpBool          `json:"hard_failure,string"`
+	HardFailure           *types.PhpBool          `json:"hard_failure,string,omitempty"`
+	Instalments           int                     `json:"instalments,string,omitempty"`
 	MarketingConsent      *types.MarketingConsent `json:"marketing_consent,string"`
 	NextRetryDate         *types.Date             `json:"next_retry_date,string"`
 	Passthrough           string                  `json:"passthrough"`
@@ -28,6 +30,7 @@ type PaymentFailed struct {
 	SubscriptionPlanID    int                     `json:"subscription_plan_id,string"`
 	UnitPrice             *types.CurrencyValue    `json:"unit_price,string"`
 	UpdateURL             string                  `json:"update_url"`
+	UserID                int                     `json:"user_id,string"`
 	PSignature            string                  `json:"p_signature" php:"-"`
 }
 
